6bff82b38ec66c8d7c07635bbe1f6450788d3e0e
Administrator <Administrator@PSPKM2826-2243>
2022-04-05 11:58:40 +0800

    Add project files.

---
+++ Form1.Designer.vb
@@ -0,0 +1,75 @@
+﻿<Global.Microsoft.VisualBasic.CompilerServices.DesignerGenerated()>
+Partial Class Form1
+    Inherits System.Windows.Forms.Form
+
+    'Form overrides dispose to clean up the component list.
+    <System.Diagnostics.DebuggerNonUserCode()>
+    Protected Overrides Sub Dispose(ByVal disposing As Boolean)
+        Try
+            If disposing AndAlso components IsNot Nothing Then
+                components.Dispose()
+            End If
+        Finally
+            MyBase.Dispose(disposing)
+        End Try
+    End Sub
+
+    'Required by the Windows Form Designer
+    Private components As System.ComponentModel.IContainer
+
+    'NOTE: The following procedure is required by the Windows Form Designer
+    'It can be modified using the Windows Form Designer.  
+    'Do not modify it using the code editor.
+    <System.Diagnostics.DebuggerStepThrough()>
+    Private Sub InitializeComponent()
+        Me.Label1 = New System.Windows.Forms.Label()
+        Me.lblOut = New System.Windows.Forms.Label()
+        Me.cbMonth = New System.Windows.Forms.ComboBox()
+        Me.SuspendLayout()
+        '
+        'Label1
+        '
+        Me.Label1.AutoSize = True
+        Me.Label1.Location = New System.Drawing.Point(42, 19)
+        Me.Label1.Name = "Label1"
+        Me.Label1.Size = New System.Drawing.Size(77, 15)
+        Me.Label1.TabIndex = 0
+        Me.Label1.Text = "Select Month"
+        '
+        'lblOut
+        '
+        Me.lblOut.AutoSize = True
+        Me.lblOut.Location = New System.Drawing.Point(42, 49)
+        Me.lblOut.Name = "lblOut"
+        Me.lblOut.Size = New System.Drawing.Size(103, 15)
+        Me.lblOut.TabIndex = 1
+        Me.lblOut.Text = "31 day in October "
+        '
+        'cbMonth
+        '
+        Me.cbMonth.FormattingEnabled = True
+        Me.cbMonth.Items.AddRange(New Object() {"1", "2", "3", "4", "5", "6", "7", "8", "9", "10", "11", "12"})
+        Me.cbMonth.Location = New System.Drawing.Point(174, 12)
+        Me.cbMonth.Name = "cbMonth"
+        Me.cbMonth.Size = New System.Drawing.Size(39, 23)
+        Me.cbMonth.TabIndex = 2
+        '
+        'Form1
+        '
+        Me.AutoScaleDimensions = New System.Drawing.SizeF(7.0!, 15.0!)
+        Me.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font
+        Me.ClientSize = New System.Drawing.Size(269, 113)
+        Me.Controls.Add(Me.cbMonth)
+        Me.Controls.Add(Me.lblOut)
+        Me.Controls.Add(Me.Label1)
+        Me.Name = "Form1"
+        Me.Text = "Form1"
+        Me.ResumeLayout(False)
+        Me.PerformLayout()
+
+    End Sub
+
+    Friend WithEvents Label1 As Label
+    Friend WithEvents lblOut As Label
+    Friend WithEvents cbMonth As ComboBox
+End Class
---
+++ Form1.vb
@@ -0,0 +1,24 @@
+﻿Public Class Form1
+    Private Sub Label2_Click(sender As Object, e As EventArgs) Handles lblOut.Click
+
+    End Sub
+
+    Private Sub ComboBox1_SelectedIndexChanged(sender As Object, e As EventArgs) Handles cbMonth.SelectedIndexChanged
+        Dim strDayMonths(,) As String = {{"31", "Januay"},
+                                         {"28 or 29", "February"},
+                                         {"31", "March"},
+                                         {"30", "April"},
+                                         {"31", "May"},
+                                         {"30", "June"},
+                                         {"31", "July"},
+                                         {"31", "August"},
+                                         {"30", "September"},
+                                         {"31", "October"},
+                                         {"30", "November"},
+                                         {"31", "Dicember"}}
+
+        Dim intMonth As Integer = cbMonth.SelectedIndex
+        lblOut.Text = strDayMonths(intMonth, 0) & "days in" &
+                                    strDayMonths(intMonth, 1)
+    End Sub
+End Class
